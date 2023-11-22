d793573034a0fe3cfa759df1f599d8026a2c0d4d
Théo.P <theopichonneau@orange.fr>
2023-11-22 14:27:50 +0100

    Problème d'affichage du message d'erreur et de l'envoie du commentaire réglé

--- DynamiserUnEspaceDeCommentaires_DocumentsDevoir/app.js
+++ DynamiserUnEspaceDeCommentaires_DocumentsDevoir/app.js
@@ -1,38 +1,40 @@
 function validateForm() {
 
-    var firstName = document.getElementById('first-name').value;
-    var lastName = document.getElementById('last-name').value;
-    var comment = document.getElementById('message').value;
-    
+  var firstName = document.getElementById('first-name').value;
+  var lastName = document.getElementById('last-name').value;
+  var comment = document.getElementById('message').value;
 
-    if (firstName === '' || lastName === '' || comment === '') {
 
-      document.getElementById('error-message').style.display = 'block';
-      /*Aucun message d'erreur ne s'affiche*/
-    } else {
-    
-        var commentElement = document.createElement('div');
-      commentElement.className = 'flex space-x-4 text-sm text-gray-500';
-      commentElement.innerHTML = `
-        <div class="flex-1 py-10 border-t border-gray-200">
-          <h3 class="font-medium text-gray-900">${firstName} + '' + ${lastName}</h3>
-          <div class="prose prose-sm mt-4 max-w-none text-gray-500">
-            <p>${comment}</p>
-          </div>
-        </div>
-      `;
+  if (firstName === '' || lastName === '' || comment === '') {
 
-      document.getElementById('comment-list').appendChild(commentElement);; /* Aucun commentaire ne s'envoie*/
-    }
+    document.getElementById('error-message').style.display = 'block';
+  } else {
+
+    document.getElementById('error-message').style.display = 'none';
+
+    addComment(firstName + ' ' + lastName, comment);
 
     document.getElementById('first-name').value = '';
     document.getElementById('last-name').value = '';
     document.getElementById('message').value = '';
-
-    document.getElementById('errorMessage').style.display = 'none';
   }
+}
 
 
+function addComment(author, content) {
 
+  var commentElement = document.createElement('div');
+  commentElement.className = 'flex space-x-4 text-sm text-gray-500';
+  commentElement.innerHTML = `
+    <div class="flex-1 py-10 border-t border-gray-200">
+      <h3 class="font-medium text-gray-900">${author}</h3>
+      <div class="prose prose-sm mt-4 max-w-none text-gray-500">
+        <p>${content}</p>
+      </div>
+    </div>
+  `;
+  
+  document.getElementById('comment-list').appendChild(commentElement);
+}
 
 
